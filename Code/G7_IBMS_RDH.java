8504e6cba56630cf2c4af06cae6d8ba753df58ed
abbottj1 <josh.abbott@live.com>
2013-02-19 17:35:21 +0000

    the class with new modifications

--- Code/G7_IBMS_RDH.java
+++ Code/G7_IBMS_RDH.java
@@ -7,23 +7,36 @@ public class G7_IBMS_RDH
   
   public static void main(String[] args)
   {
-    database.openBusDatabase();
-    int driverID = 2064;
-    Holiday holidayRequest = new Holiday(20,2,2013,19,2,2013);
+    int driverID = Integer.parseInt(args[0]);
+    int srtDay = Integer.parseInt(args[1]);
+    int srtMonth = Integer.parseInt(args[2]);
+    int srtYear = Integer.parseInt(args[3]);
+    int endDay = Integer.parseInt(args[4]);
+    int endMonth = Integer.parseInt(args[5]);
+    int endYear = Integer.parseInt(args[6]);
+    
+    BusRoot.openBusDatabase();
+    Holiday holidayRequest = new Holiday(srtDay,srtMonth,srtYear,
+					 endDay,endMonth,endYear);
     int requestedDays = holidayRequest.getNoOfDays();
     while (requestedDays <= 0)
     {
       System.out.println("The start date must be before the end date");
       System.out.println("Please enter them again");
-      holidayRequest.setStartDate(20,2,2013);
-      holidayRequest.setEndDate(27,2,2013);
-      requestedDays = holidayRequest.getNoOfDays();
+      return;
     }
     System.out.println(requestedDays+ " reqDays");
     if(hasAvalibleHoliday(driverID, requestedDays))
-      System.out.println("This request is valid");
+      System.out.println("You have enough holiday");
     else
-      System.out.println("This request is not valid");
+      System.out.println("This request is not valid - you don't have enough"+
+	  " holiday");
+    
+    if(holidayRequest.daysAway() >= 7)
+      System.out.println("The start date is 7 or more days away");
+    else
+      System.out.println("This request is not valid - it's less than 7 days"+
+	  " away");
     
   }//main
   
