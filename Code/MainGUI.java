bc024d3f54c5a98ec145ee43d02b71ad015aca4d
Daniela <daniela.florescu91@yahoo.com>
2013-02-20 16:31:32 +0000

    changed the file structure

---
+++ Code/MainGUI.java
@@ -0,0 +1,15 @@
+class MainGUI{
+   
+  private static String title = "G7 - IBMS System";
+ 
+  public static void main(String[] paramArrayOfString){
+
+    //Run the starting screen
+    new WelcomeScreen(title);
+
+    //new ControllerRequestViewScreen("G7 - IBMS System | Controller");
+  
+  }//main
+
+}//class
+
