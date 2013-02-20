9982d72f94633444047901c98aaf5bdd435bad71
mkehayov92 <mkehayov92@gmail.com>
2013-02-20 07:26:46 +0000

    updated

--- GUI/ControllerRequestViewScreen.java
+++ GUI/ControllerRequestViewScreen.java
@@ -6,8 +6,9 @@ class ControllerRequestViewScreen extends JFrame
                                   implements ActionListener{
 
   //Declare the components
-  JLabel jLabelTimetable;
-  JPanel contentPanel;
+  JLabel jLabelTimetable, jLabelSelectedView;
+  JPanel contentPanel, mainContentPanel, requestPanel;
+  JScrollPane jScrollPanel;
   JMenuBar mainMenuBar;
   JMenu jMenuFile, jMenuView;
   JMenuItem jMItemSave, jMItemPrint, jMItemExit;
@@ -16,6 +17,7 @@ class ControllerRequestViewScreen extends JFrame
   //Declate the colors
   Color layoutBgClr = new Color(255, 255, 255);
   Color lblFgClr = new Color(150, 150, 150);
+  Color reqBgClr = new Color(140, 140, 140);
  
   public ControllerRequestViewScreen(String paramString){
 
@@ -31,9 +33,14 @@ class ControllerRequestViewScreen extends JFrame
     jMenuView = new JMenu("View");
     jMenuView.setMnemonic(KeyEvent.VK_V);
 
+    jLabelSelectedView = new JLabel("Selected View: Holidays");
+    jLabelSelectedView.setForeground(lblFgClr);
+
     //Add the two menus to the menu bar
     mainMenuBar.add(jMenuFile);
     mainMenuBar.add(jMenuView);
+    mainMenuBar.add(Box.createHorizontalGlue());
+    mainMenuBar.add(jLabelSelectedView);
 
     //Create the menu items
     jMItemSave = new JMenuItem("Save");
@@ -63,18 +70,65 @@ class ControllerRequestViewScreen extends JFrame
     jMenuView.add(this.jMItemHolidays);
     jMenuView.add(this.jMItemDrivers);
 
-    //Create the label
-    jLabelTimetable = new JLabel("Controller Request label");
-    jLabelTimetable.setForeground(this.lblFgClr);
+    //Create the mainContent panel
+    mainContentPanel = new JPanel();
+    mainContentPanel.setLayout(new GridLayout(0, 3, 20, 20));
+    mainContentPanel.setBackground(layoutBgClr);
  
+    //Create the scrollpanel
+    jScrollPanel = new JScrollPane(mainContentPanel);
+    jScrollPanel.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+    jScrollPanel.setBackground(layoutBgClr);
+
     //Create the content panel
     contentPanel = new JPanel();
     contentPanel.setPreferredSize(new Dimension(600, 300));
-    contentPanel.setLayout(new GridLayout(1, 1));
-    contentPanel.setBackground(this.layoutBgClr);
+    contentPanel.setLayout(new BorderLayout());
+    contentPanel.setBackground(layoutBgClr);
+
+    JPanel[] reqPanels = new JPanel[10];
+    JButton[] aBtns = new JButton[10];
+    JButton[] dBtns = new JButton[10];
+
+    //Create the approve buttons 
+    for(int i = 0; i < 10; i++){
+      aBtns[i] = new JButton("Approve");
+      aBtns[i].setForeground(lblFgClr);
+      aBtns[i].setBackground(layoutBgClr);
+      aBtns[i].setActionCommand("approve" + i);
+      aBtns[i].addActionListener(this);
+    }
+    
+    //Create the decline buttons
+    for(int i = 0; i < 10; i++){
+      dBtns[i] = new JButton("Decline");
+      dBtns[i].setForeground(lblFgClr);
+      dBtns[i].setBackground(layoutBgClr);
+      dBtns[i].setActionCommand("decline" + i);
+      dBtns[i].addActionListener(this);
+    }
+
+    //Create the requestPanels
+    for(int i = 0; i < 10; i++){
+      reqPanels[i] = new JPanel();
+      reqPanels[i].setLayout(new GridLayout(6, 1));
+      reqPanels[i].setBackground(layoutBgClr);
+
+      reqPanels[i].add(new JLabel("Start: 14/7/2013 "));
+      reqPanels[i].add(new JLabel("End: 13/8/2013"));
+      reqPanels[i].add(new JLabel("DriverID: #213123123"));
+      reqPanels[i].add(new JLabel("Status: pending"));
+      reqPanels[i].add(aBtns[i]);
+      reqPanels[i].add(dBtns[i]);
+    }
+
+    //Add the requestPanels to the mainContentPanel
+    for(int i = 1; i < 10; i++){
+      mainContentPanel.add(reqPanels[i]);
+    }
 
     //Add the label to the content panel
-    contentPanel.add(this.jLabelTimetable);
+    contentPanel.add(jScrollPanel, BorderLayout.CENTER);
 
     //Resize and position the window
     Dimension localDimension = Toolkit.getDefaultToolkit().getScreenSize();
@@ -123,6 +177,23 @@ class ControllerRequestViewScreen extends JFrame
     else if ("drivers".equals(actionCmd)){
       System.out.println("Drivers menu selected");
     }
+
+    for(int i = 0; i < 10; i++){
+      String aBtnAction = "approve"+i;
+      String dBtnAction = "decline"+i;
+
+      if(aBtnAction.equals(actionCmd)){
+        //Logic for approve. i is unique
+        System.out.println("" + aBtnAction + " pressed");
+      }
+      
+      if(dBtnAction.equals(actionCmd)){
+        //Logic for decline. i is unique
+        System.out.println("" + dBtnAction + " pressed");
+      }
+    }//for
+
+
   }//actionPerformed
 }//class
 
