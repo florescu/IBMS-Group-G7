c766bb274eb9b55affe1cbb9d2ef4c0003534f82
abbottj1 <josh.abbott@live.com>
2013-05-06 23:14:49 +0100

    Journey planner

--- Code/IBMS-Group-G7/src/TestJourney.java
+++ Code/IBMS-Group-G7/src/TestJourney.java
@@ -3,8 +3,25 @@ public class TestJourney
 {
 	public static void main(String[] args)
 	{
+		Journey j = new Journey();
+		/*
 		database.openBusDatabase();
-		Journey journey = new Journey(775,779,0);
+		int[] routes = BusStopInfo.getRoutes();
+		for(int i = 0; i < routes.length; i++)
+		{
+			System.out.println(routes[i]);
+			int[] stops = TimetableInfo.getServiceTimes(routes[i],10);
+			for(int j = 0; j < stops.length; j++)
+			{
+				int stop = TimetableInfo.getTimingPoint(10, stops[j]);
+				System.out.println(BusStopInfo.getFullName(stop));
+				System.out.println(stops[j]);
+			}
+			System.out.println();
+		}
+		//Journey journey = new Journey(775,779,0);
+		 * 
+		 */
 	}
 
 }
