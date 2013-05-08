03dbdae8fcb1fae8694aa787c02083b63f4454d3
abbottj1 <josh.abbott@live.com>
2013-05-08 18:44:21 +0100

    JOurney planer scrren

--- Code/IBMS-Group-G7/src/TestJourney.java
+++ Code/IBMS-Group-G7/src/TestJourney.java
@@ -4,8 +4,8 @@ public class TestJourney
 	public static void main(String[] args)
 	{
 		database.openBusDatabase();
-		Journey j = new Journey(1,11,900);
-		
+		Journey j = new Journey(1,13,900);
+		new JourneyPlannerScreen("G7 - IBMS System | Passenger - Information");
 		
 	}
 
